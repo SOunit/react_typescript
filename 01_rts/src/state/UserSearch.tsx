36c943b07cba3a0cea1cd2085b826a2a58aef04c
SOunit <workbookwork7@gmail.com>
2021-05-08 15:51:21 +0900

    add setState interface sample

---
+++ 01_rts/src/state/UserSearch.tsx
@@ -0,0 +1,40 @@
+import { useState } from 'react';
+
+const users = [
+  {
+    name: 'Jack',
+    age: 38,
+  },
+  {
+    name: 'Rebecca',
+    age: 38,
+  },
+  {
+    name: 'Kevin',
+    age: 12,
+  },
+];
+
+const UserSearch: React.FC = () => {
+  const [name, setName] = useState('');
+  const [user, setUser] = useState<{ name: string; age: number } | undefined>();
+
+  const onClick = () => {
+    const foundUser = users.find((user) => {
+      return user.name.toLowerCase() === name.toLowerCase();
+    });
+    setUser(foundUser);
+  };
+
+  return (
+    <div>
+      User Search
+      <input value={name} onChange={(e) => setName(e.target.value)} />
+      <button onClick={onClick}>Find User</button>
+      <div>{user && user.name}</div>
+      <div>{user && user.age}</div>
+    </div>
+  );
+};
+
+export default UserSearch;
