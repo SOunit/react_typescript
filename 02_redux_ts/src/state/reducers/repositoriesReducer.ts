1c8b2fcbde58019d5fa281281954c09e98d60a13
SOunit <workbookwork7@gmail.com>
2021-05-08 23:46:21 +0900

    add repositories reducer

---
+++ 02_redux_ts/src/state/reducers/repositoriesReducer.ts
@@ -0,0 +1,23 @@
+import { string } from 'yargs';
+
+interface RepositoriesState {
+  loading: boolean;
+  error: string | null;
+  data: string[];
+}
+
+const reducer = (state: RepositoriesState, action: any) => {
+  switch (action.type) {
+    case 'search_repositories':
+      // new search request reset state
+      return { loading: true, error: null, data: [] };
+    case 'search_repositories_success':
+      return { loading: false, error: null, data: action.payload };
+    case 'search_repositories_error':
+      return { loading: false, error: action.payload, data: [] };
+    default:
+      return state;
+  }
+};
+
+export default reducer;
