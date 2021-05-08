4e6a06afb4e82f60c0f14da5b578b4d7ddf0f379
SOunit <workbookwork7@gmail.com>
2021-05-08 15:00:00 +0900

    add functional component sample with typescript

--- 01_rts/src/props/Child.tsx
+++ 01_rts/src/props/Child.tsx
@@ -2,6 +2,17 @@ interface ChildProps {
   color: string;
 }
 
+// Not React Component yet for TypeScript
+// this receive props and returns jsx, that's all.
 export const Child = ({ color }: ChildProps) => {
   return <div>{color}</div>;
 };
+
+// Function Component with TypeScript
+export const ChildAsFC: React.FC<ChildProps> = ({ color }) => {
+  return <div>{color}</div>;
+};
+
+// FunctionComponent style offers ReactComponent properties.
+// choice for which stytle you use is optional.
+ChildAsFC.displayName;
