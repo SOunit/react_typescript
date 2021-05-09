c028e31e96fd2798ff544236f69dbc01d9891eb9
SOunit <workbookwork7@gmail.com>
2021-05-10 07:11:34 +0900

    initial commit

---
+++ 04_jbook/src/index.tsx
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  const [input, setInputt] = useState('');
+  const [code, setCode] = useState('');
+
+  const onClick = () => {
+    console.log(input);
+  };
+
+  return (
+    <div>
+      <textarea
+        onChange={(e) => setInputt(e.target.value)}
+        value={input}
+      ></textarea>
+      <div>
+        <button onClick={onClick}>Submit</button>
+      </div>
+      <pre>{code}</pre>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector('#root'));
