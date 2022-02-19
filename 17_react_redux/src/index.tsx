439bed0c656f643c0d43c07550e9ce844184a5ef
SOunit <workbookwork7@gmail.com>
2022-02-18 21:20:05 -0800

    add new project

---
+++ 17_react_redux/src/index.tsx
@@ -0,0 +1,17 @@
+import "bulmaswatch/superhero/bulmaswatch.min.css";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import TextEditor from "./components/text-editor";
+import { store } from "./state";
+
+const App = () => {
+  return (
+    <Provider store={store}>
+      <div>
+        <TextEditor />
+      </div>
+    </Provider>
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
