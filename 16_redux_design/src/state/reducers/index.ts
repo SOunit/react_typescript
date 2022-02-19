3c7fdba5008f4a342c556e34e20f1d76c1130531
SOunit <workbookwork7@gmail.com>
2022-02-18 20:08:07 -0800

    add redux sample

---
+++ 16_redux_design/src/state/reducers/index.ts
@@ -0,0 +1,11 @@
+import { combineReducers } from "redux";
+import cellsReducer from "./cellsReducer";
+
+const reducers = combineReducers({
+  cells: cellsReducer,
+});
+
+export default reducers;
+
+// for useSelector hook
+export type RootState = ReturnType<typeof reducers>;
